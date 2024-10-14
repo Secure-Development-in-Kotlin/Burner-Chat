c762e7e94e5228d9700bb7342bb24e66c0374091
Juanakin18 <juaniteje@gmail.com>
2024-10-14 10:04:30 +0200

    Primer commit de la rama

--- BurnerChat/app/src/main/java/com/example/burnerchat/MainActivity.kt
+++ BurnerChat/app/src/main/java/com/example/burnerchat/MainActivity.kt
@@ -7,6 +7,9 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    /**
+     * Prueba
+     */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
