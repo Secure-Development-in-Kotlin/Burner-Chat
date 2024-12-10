5fdd3aed50718b68cf1adb447968e6f9a8e12210
Juanakin18 <juaniteje@gmail.com>
2024-12-10 19:12:14 +0100

    Arreglos varios

--- BurnerChat/app/src/main/java/com/example/burnerchat/webRTC/model/messages/Message.kt
+++ BurnerChat/app/src/main/java/com/example/burnerchat/webRTC/model/messages/Message.kt
@@ -3,9 +3,10 @@ package com.example.burnerchat.webRTC.model.messages
 import com.example.burnerchat.webRTC.model.chats.Chat
 import com.example.burnerchat.webRTC.model.users.User
 import java.time.LocalDate
+import java.time.LocalDateTime
 
 interface Message {
-    public fun getSentDate(): LocalDate
+    public fun getSentDate(): LocalDateTime
     public fun getContent(): String
     public fun getUser(): User
     public fun getChat(): Chat
