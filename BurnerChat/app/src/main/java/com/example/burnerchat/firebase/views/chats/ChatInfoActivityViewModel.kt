4bec0ccc5b73efecb64b1ed7cf90750e4ebb4c66
Juanakin18 <113385286+Juanakin18@users.noreply.github.com>
2025-01-05 17:03:37 +0100

    Edición de chats funcional (#54)

--- BurnerChat/app/src/main/java/com/example/burnerchat/firebase/views/chats/ChatInfoActivityViewModel.kt
+++ BurnerChat/app/src/main/java/com/example/burnerchat/firebase/views/chats/ChatInfoActivityViewModel.kt
@@ -1,17 +1,25 @@
 package com.example.burnerchat.firebase.views.chats
 
+import android.graphics.Bitmap
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.burnerchat.BurnerChatApp
 import com.example.burnerchat.firebase.model.chats.Chat
+import com.example.burnerchat.firebase.repositories.ImageUtils
 import kotlinx.coroutines.launch
 
 class ChatInfoActivityViewModel : ViewModel() {
     private val database = BurnerChatApp.appModule.usersRepository
     private val chatsDB = BurnerChatApp.appModule.chatsRepository
 
+    fun init(){
+        viewModelScope.launch {
+
+        }
+    }
     private val _chat = MutableLiveData<Chat>()
     val chat : LiveData<Chat>
         get() = _chat
@@ -34,5 +42,19 @@ class ChatInfoActivityViewModel : ViewModel() {
         val emails = chat.value?.participants!!
         var users = database.getUsersByEmail(emails)
         _usersDBList.value = users
+    }
+
+    fun setIcon(bitmap: Bitmap) {
+        chat.value?.imageUrl = ImageUtils.convertToBase64(bitmap)
+        setChat(chat.value!!)
+    }
+
+    fun setNombre(name: String){
+        chat.value?.name = name
+        setChat(chat.value!!)
+    }
+
+    fun updateChat(){
+        chatsDB.updateChat(chat.value!!)
     }
 }
